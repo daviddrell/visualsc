57c2002cc47f9f49a042ce05ef17e43341bc52a2
Matthew Zhan <matthewzhan@utexas.edu>
2015-08-26 10:51:07 -0500

    Fixed bug dealing with auto resize not saving to the data model.

--- scgraphicsview/scgraphicsview.cpp
+++ scgraphicsview/scgraphicsview.cpp
@@ -402,6 +402,8 @@ void SCGraphicsView::handleAutoResize(StateBoxGraphic* stateBoxGraphic)
                 {
                     StateBoxGraphic* sbg = sbgs.at(i);
                     sbg->setPosAndUpdateAnchors(sbg->pos()-QPointF(minX-25,minY-25));
+                    // update the datamodel for the position and size.
+                    sbg->graphicHasChanged();
                 }
 
                 // all children are inside of the state machine, so we can call this again
